a7d8b2fe94b283c46cc0d1bda0c17e2bc9ee821d
TheLowerRealmGame <thelowerrealm@gmail.com>
2019-05-14 19:17:13 +0100

    Added a simplified example with Inheritance and abstract records

---
+++ Examples/TileData.cs
@@ -0,0 +1,48 @@
+﻿using Tyd;
+
+namespace Example
+{
+
+    public class TileData
+    {
+        public int Id { get; private set; }
+
+        public string Tag { get; private set; }
+        public string Name { get; private set; }
+
+        public bool BackTile { get; private set; }
+        public bool Transparent { get; private set; }
+        public bool Solid { get; private set; }
+        public bool Climbable { get; private set; }
+        public bool Mineable { get; private set; }
+
+        public TileData() { }
+
+        public void ReadData(TydTable data)
+        {
+            // Get id
+            Id = GetTydString(data, "id").IntValue;
+            
+            // Get tag
+            Tag = GetTydString(data, "tag").Value;
+            // Get Name
+            Name = GetTydString(data, "name").Value;
+
+            // Get back tile flag
+            BackTile = GetTydString(data, "backTile").BoolValue;
+            // Get transparent flag
+            Transparent = GetTydString(data, "transparent").BoolValue;
+            // Get solid flag
+            Solid = GetTydString(data, "solid").BoolValue;
+            // Get climbable flag
+            Climbable = GetTydString(data, "climbable").BoolValue;
+            // Get mineable flag
+            Mineable = GetTydString(data, "mineable").BoolValue;
+        }
+
+        private TydString GetTydString(TydTable tydTable, string name)
+        {
+            return tydTable[name] as TydString;
+        }
+    }
+}
---
+++ Examples/TileDataReader.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using Tyd;
+
+namespace Example
+{
+    public class TileDataReader
+    {
+        public static TydFile LoadTilesConfiguration(string filePath)
+        {
+            // Load the file
+            TydFile file = TydFile.FromFile(filePath);
+
+            // Resolve inheritance
+            Inheritance.Initialize();
+            try
+            {
+                TydDocument doc = file.DocumentNode;
+
+                Inheritance.RegisterAllFrom(doc);
+
+                Inheritance.ResolveAll();
+            }
+            finally
+            {
+                Inheritance.Complete();
+            }
+
+            return file;
+        }
+
+        public static List<TileData> LoadTileData(string filePath)
+        {
+            List<TileData> tilesData = new List<TileData>();
+
+            TydFile file = LoadTilesConfiguration(filePath);
+
+            TileData data;
+            TydTable tydTable;
+
+            foreach(TydNode node in file.DocumentNode.Nodes)
+            {
+                tydTable = node as TydTable;
+
+                // Only read the data if the node is a table and is not abstract
+                if(tydTable != null
+                    && !tydTable.AttributeAbstract)
+                {
+                    data = new TileData();
+
+                    data.ReadData(tydTable);
+
+                    UnityEngine.Debug.Log(data.Name);
+
+                    tilesData.Add(data);
+                }
+            }
+
+            return tilesData;
+        }
+    }
+}
