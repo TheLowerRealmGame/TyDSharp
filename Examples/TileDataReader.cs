b6ecb083e7b8c9e1507940638277395a774008e6
TheLowerRealmGame <thelowerrealm@gmail.com>
2019-05-14 19:18:39 +0100

    Clean up

--- Examples/TileDataReader.cs
+++ Examples/TileDataReader.cs
@@ -49,8 +49,6 @@ namespace Example
 
                     data.ReadData(tydTable);
 
-                    UnityEngine.Debug.Log(data.Name);
-
                     tilesData.Add(data);
                 }
             }
